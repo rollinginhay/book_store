3a5097b93c8b842b63870200c9d56f502814a19b
rollinginhay <.>
2025-08-30 04:30:54 +0700

    first commit

---
+++ src/test/java/hn_152/bookstore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package hn_152.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
