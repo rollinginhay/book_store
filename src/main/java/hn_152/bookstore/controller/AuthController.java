2bfff4ffb4fda2e32725760f6be2c0de47207541
rollinginhay <.>
2025-09-11 03:35:03 +0700

    complete basic auth

---
+++ src/main/java/hn_152/bookstore/controller/AuthController.java
@@ -0,0 +1,40 @@
+package hn_152.bookstore.controller;
+
+import hn_152.bookstore.model.dto.request.LoginRequest;
+import hn_152.bookstore.model.dto.request.RegisterRequest;
+import hn_152.bookstore.model.dto.response.AuthResponse;
+import hn_152.bookstore.service.auth.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/v1/auth")
+@RestController
+@Tag(name = "Non-OAuth authentication", description = "Auth by username and password")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @Operation(description = "Login with username and password")
+    @ApiResponse(responseCode = "200", description = "Login success", content = @Content(schema = @Schema(implementation = AuthResponse.class)))
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
+        return ResponseEntity.ok(authService.login(loginRequest));
+    }
+
+    @Operation(description = "Manually register a new user")
+    @ApiResponse(responseCode = "200", description = "Register success", content = @Content(schema = @Schema(implementation = AuthResponse.class)))
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponse> register(@Valid @RequestBody RegisterRequest registerRequest) {
+        return ResponseEntity.ok(authService.register(registerRequest));
+    }
+}
